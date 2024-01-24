f5952b7a19800affefb54d91225e66fbcc914538
Cleverton Costa <cdacostaalves9@gmail.com>
2024-01-24 19:13:13 -0300

    dando mais opções ao código usando else if

---
+++ else-if.js
@@ -0,0 +1,9 @@
+const temperature = 36
+
+if (temperature === 36) {
+    console.log("Você está saudavel")
+}else if (temperature > 36) {
+    console.log("Está com febre")
+}else {
+    console.log("Está com hipotermia")
+}
